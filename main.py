d218c041e2f0a3d2163027f4385727213c7c8eda
Renan Araújo <awkrenan@gmail.com>
2023-06-05 10:15:04 -0300

    added file browser

--- main.py
+++ main.py
@@ -1,58 +1,57 @@
 import curses
 
+import os
+
+def get_files(directory):
+    files = os.listdir(directory)
+
+    return [f for f in files if os.path.isfile(os.path.join(directory, f))]
+
+def draw_file_list(stdscr, file_list, selected_index):
+    stdscr.clear()
+
+    height, width = stdscr.getmaxyx()
+
+    for i, file_name in enumerate(file_list):
+        if i == selected_index:
+            stdscr.attron(curses.A_REVERSE)
+        else:
+            stdscr.attroff(curses.A_REVERSE)
+
+        stdscr.addstr(i, 0, file_name)
+
+    stdscr.refresh()
+
+
+
+
 def main(stdscr):
 
-    # setup screen
-    curses.curs_set(0) # hide cursor
-    stdscr.nodelay(1) # make getch() non-blocking
-    stdscr.timeout(100) # set getch() timeout in ms
+    stdscr.keypad(True)
+    curses.curs_set(0)
 
-    # main loop
+    current_dir = os.path.expanduser('~/Música/')
+    file_list = get_files(current_dir)
+    selected_index = 0
+
     while True:
         
-        # clear screen
-        stdscr.clear()
-        
-        # get user input
+        draw_file_list(stdscr, file_list, selected_index)
+
         key = stdscr.getch()
 
-        # quit program if the user presses q
-        if key == ord('q'):
-            break
-        
-        # perform actions based on user input
-        if key == ord('p'):
-            play_music()
-        elif key == ord('s'):
-            stop_music()
-        elif key == ord('n'):
-            next_track()
-        elif key == ord('p'):
-            previous_track()
+        if key == curses.KEY_UP:
+            selected_index = max(0, selected_index - 1)
+        elif key == curses.KEY_DOWN:
+            selected_index = min(len(file_list) - 1, selected_index + 1)
+        elif key == ord('\n'): # enter key
+            selected_file = file_list[selected_index]
+            #play the song
 
-        # render screen
-        render_interface(stdscr)
+        new_dir = os.path.join(current_dir, file_list[selected_index])
+        if os.path.isdir(new_dir):
+            current_dir = new_dir
+            file_list = get_files(current_dir)
+            selected_index = 0
 
-        # refresh screen
-        stdscr.refresh()
-
-def render_interface(stdscr):
-    # render interface here
-    # ex: stdscr.addstr(0, 0, "rattle music player")
-    # use curses functions to position text and elements on the screen
-
-def play_music():
-    # to do
-
-def stop_music():
-    # to do
-
-def next_track():
-    # to do
-
-def previous_track():
-    # to do
-
-if __name__ == "__main__":
-    curses.wrapper(main)
-
+curses.wrapper(main)
